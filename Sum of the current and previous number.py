70bd51f36946bee2a7d7fea818bcaa402aba6b85
Gerald <geraldonod@gmail.com>
2024-01-24 11:43:05 +0800

    get the sum

--- Sum of the current and previous number.py
+++ Sum of the current and previous number.py
@@ -8,5 +8,7 @@ for i in range(10):
 
     current_num = i
 #get the sum by adding current and previous number
+    number_sum = current_num + previous_num
 
 #print out result
+    print(f"Current: {current_num}, Previous: {previous_num}, Sum: {number_sum}")
