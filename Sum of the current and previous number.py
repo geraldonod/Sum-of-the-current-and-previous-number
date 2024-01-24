720e3136afde6275e8531e282ae2396667d565ac
Gerald <geraldonod@gmail.com>
2024-01-24 11:40:29 +0800

    if else statement

--- Sum of the current and previous number.py
+++ Sum of the current and previous number.py
@@ -1,7 +1,12 @@
 #for loop for the first 10 numbers
 for i in range(10):
 #if else statement to identify current and previous number
+    if i == 0:
+        previous_num = 0
+    else:
+        previous_num = i - 1
 
+    current_num = i
 #get the sum by adding current and previous number
 
 #print out result
